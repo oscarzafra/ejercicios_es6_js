c2819c09d242069069bb054442ffe8b2abce72b4
oscarzafra <oscarzafra35@gmail.com>
2024-10-08 09:40:00 +0200

    Ejercicio1

---
+++ ejercicio1/ejercicio1.2.js
@@ -0,0 +1,12 @@
+// 1.2 En base al siguiente javascript, usa destructuring para crear 3 variables llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormenteimprimelo por consola.
+
+const fruits = ['Banana', 'Strawberry', 'Orange'];
+
+const firstFruit = fruits[0];
+const secondFruit = fruits[1];
+
+const [fruit1, fruit2, fruit3] = fruits;
+
+console.log(fruit1);
+console.log(fruit2);
+console.log(fruit3);
